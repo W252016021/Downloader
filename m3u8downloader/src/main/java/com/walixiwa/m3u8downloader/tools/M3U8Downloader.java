354c0c9c2133c8ff74b6fdecac278371658cda7a
W252016021 <252016021@qq.com>
2019-09-19 05:18:41 +0800

    增加是否正在下载的判断

--- m3u8downloader/src/main/java/com/walixiwa/m3u8downloader/tools/M3U8Downloader.java
+++ m3u8downloader/src/main/java/com/walixiwa/m3u8downloader/tools/M3U8Downloader.java
@@ -4,6 +4,7 @@ import android.text.TextUtils;
 import android.util.Log;
 
 import com.walixiwa.m3u8downloader.tools.entity.M3U8Model;
+import com.walixiwa.m3u8downloader.tools.entity.TaskDeleteListener;
 
 import java.io.ByteArrayOutputStream;
 import java.io.File;
@@ -227,4 +228,62 @@ public class M3U8Downloader {
     }
 
 
+    public void deleteTask(final TaskDeleteListener taskDeleteListener) {
+        new Thread(new Runnable() {
+            @Override
+            public void run() {
+                taskDeleteListener.onStart();
+                delAllFile(localFile);
+                taskDeleteListener.onFinish();
+            }
+        }).start();
+
+    }
+
+
+    public static boolean delAllFile(String path) {
+        boolean flag = false;
+        try {
+            File file = new File(path);
+            if (!file.exists()) {
+                return flag;
+            }
+            if (!file.isDirectory()) {
+                return flag;
+            }
+            String[] tempList = file.list();
+            File temp = null;
+            for (int i = 0; i < tempList.length; i++) {
+                if (path.endsWith(File.separator)) {
+                    temp = new File(path + tempList[i]);
+                } else {
+                    temp = new File(path + File.separator + tempList[i]);
+                }
+                if (temp.isFile()) {
+                    temp.delete();
+                }
+                if (temp.isDirectory()) {
+                    delAllFile(path + "/" + tempList[i]);//先删除文件夹里面的文件
+                    delFolder(path + "/" + tempList[i]);//再删除空文件夹
+                    flag = true;
+                }
+            }
+        } catch (Exception e) {
+            e.printStackTrace();
+            flag = false;
+        }
+        return flag;
+    }
+
+    private static void delFolder(String folderPath) {
+        try {
+            delAllFile(folderPath); //删除完里面所有内容
+            String filePath = folderPath;
+            filePath = filePath.toString();
+            File myFilePath = new File(filePath);
+            myFilePath.delete(); //删除空文件夹
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
 }
---
+++ m3u8downloader/src/main/java/com/walixiwa/m3u8downloader/tools/entity/TaskDeleteListener.java
@@ -0,0 +1,7 @@
+package com.walixiwa.m3u8downloader.tools.entity;
+
+public interface TaskDeleteListener {
+    void onStart();
+
+    void onFinish();
+}
